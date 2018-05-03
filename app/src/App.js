5e0b6b4f33cc129072ff961f27a02ac11010c002
Edson Lucas <edl_@live.com>
2018-05-03 15:35:26 -0300

    site completed

--- app/src/App.js
+++ app/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         {/* HEADER START */}
         <header className="header-section clearfix">
           {/* Start Navigation */}
-          <nav id="mainNav" className="navbar navbar-expand-lg fixed-top hornbill-navbar bg-wine">
+          <nav id="mainNav" className="navbar navbar-expand-lg fixed-top hornbill-navbar bg-wine d-lg-block d-md-block d-sm-none d-none">
             <div className="container">
               <a className="js-scroll-trigger" href="index.html">
                 <img src="assets/img/logo-white.png" className="navbar-brand" />
@@ -26,13 +26,13 @@ class App extends Component {
               <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav ml-auto hornbill-nav">
                   <li className="nav-item">
-                    <a className="nav-link js-scroll-trigger" href="#services">Início</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link js-scroll-trigger" href="#features">Conheça o Descarte Verde</a>
+                    <a className="nav-link js-scroll-trigger" href="#home">Início</a>
                   </li>
                   <li className="nav-item">
                     <a className="nav-link js-scroll-trigger" href="#about">Como funciona</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link js-scroll-trigger" href="#features">Conheça o Descarte Verde</a>
                   </li>
                 </ul>
               </div>
@@ -42,9 +42,9 @@ class App extends Component {
         </header>
         {/* END HEADER */}
         {/* MAIN SECTION START */}
-        <main className="main-section">
+        <main id="home" className="main-section">
           {/* Banner Section Start */}
-          <div className="banner-section banner-seven animate-cloud p-top-100 p-sm-top-50 bg-wine">
+          <div className="banner-section banner-seven animate-cloud p-top-100 p-sm-top-0 bg-wine">
             <div className="animate-cloud-effects" />
             <div className="container relative z-3">
               <div className="row ">
@@ -74,7 +74,7 @@ class App extends Component {
           </div>
           {/* END of Banner Section */}
           {/* Service Section Start */}
-          <section id="services" className="service-section p-top-80">
+          <section id="about" className="service-section p-top-80">
             <div className="container">
               <div className="row">
                 <div className="col-md-6 single-service fadeInUp">
@@ -97,7 +97,7 @@ class App extends Component {
           </section>
           {/* END of Service Section */}
           {/* Video Section Start */}
-          <section className="video-section" id="features">
+          <section id="features" className="video-section">
             <div className="card-group">
               <div className="card bg-phone-at-hands">
                 <div className="video">
@@ -132,73 +132,6 @@ class App extends Component {
             {/* End card group */}
           </section>
           {/* END of Video Section */}
-          {/* About Section Start */}
-          <div id="about" className="about-section bg-lighter-reverse pattern-top p-top-150 p-sm-top-80">
-            <div className="container">
-              <div class="col-sm-12">
-                <div class="faq-heading m-bot-100 m-sm-bot-40">
-                  <div class="big-brand-logo">
-                    <img src="assets/img/logo-black.png" class="img-fluid" />
-                  </div>
-                </div>
-              </div>
-              {/* More Features Start */}
-              <section className="more-feature-section row p-top-100 p-bot-100 p-sm-bot-60">
-                <div className="col-md-5 p-sm-bot-30">
-                  <div className="more-features-phones m-sm-bot-45">
-                    <div className="phone-slide ">
-                      <div className="swiper-container phone-slide-gallery">
-                        <div className="swiper-wrapper">
-                          <div className="swiper-slide"><img src="assets/img/gallery/screen-1.png" alt="Image" className="img-fluid" /></div>
-                          <div className="swiper-slide"><img src="assets/img/gallery/screen-2.png" alt="Image" className="img-fluid" /></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                {/* End of col left */}
-                <div className="col-md-7">
-                  <div className="promo-heading m-bot-35 fadeIn">
-                    <h2>Amazing Features.</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis rhoncus ante, eget molestie dolor. Duis tellus magna, malesuada vitae velit in, ultrices fringilla metus. Nulla facilisi.</p>
-                  </div>
-                  <ul className="more-features-list">
-                    <li className="more-single-feature fadeIn">
-                      <div className="icon">
-                        <span className="bi bi-alien" />
-                      </div>
-                      <h4>Produtos Descartáveis</h4>
-                      <p>Sed vehculia semiper sapen quis, gravida massa cursus luctus. Cras at feugiat urna. Praesent tempus sed quam vel ornare magna.</p>
-                    </li>
-                    <li className="more-single-feature fadeIn">
-                      <div className="icon">
-                        <span className="bi bi-badge2" />
-                      </div>
-                      <h4>Pontos de Entrega</h4>
-                      <p>Sed vehculia semiper sapen quis, gravida massa cursus luctus. Cras at feugiat urna. Praesent tempus sed quam vel ornare magna.</p>
-                    </li>
-                    <li className="more-single-feature fadeIn">
-                      <div className="icon">
-                        <span className="bi bi-brush" />
-                      </div>
-                      <h4>Color schemes</h4>
-                      <p>Sed vehculia semiper sapen quis, gravida massa cursus luctus. Cras at feugiat urna. Praesent tempus sed quam vel ornare magna.</p>
-                    </li>
-                    <li className="more-single-feature fadeIn">
-                      <div className="icon">
-                        <span className="bi bi-ufo" />
-                      </div>
-                      <h4>pure &amp;simgple</h4>
-                      <p>Sed vehculia semiper sapen quis, gravida massa cursus luctus. Cras at feugiat urna. Praesent tempus sed quam vel ornare magna.</p>
-                    </li>
-                  </ul>
-                </div>
-                {/* End of col right */}
-              </section>
-              {/* More Features End */}
-            </div>
-          </div>
-          {/* END of About Section */}
           {/* Promo Download Section Start */}
           <section className="promo-download-section bg-people bg-fixed overlay-wine p-top-60 p-sm-top-0">
             <div className="container">
@@ -234,22 +167,7 @@ class App extends Component {
         </main>
         {/* END MAIN SECTION */}
         {/* FOOTER START */}
-        <footer className="footer-section bg-lighter m-top-60 m-sm-top-60">
-          <div className="footer-top m-bot-30 m-sm-bot-30">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-12 footer-widget fadeIn text-lg-left">
-                  <div className="footer-logo">
-                    <a href="index.html">
-                      <img src="assets/img/logo.png" alt="img" className="img-fluid" style={{display: 'block', margin: 'auto'}} />
-                    </a>
-                  </div>
-                </div>
-              </div>
-              {/* end row */}
-            </div>
-          </div>
-          {/* end footer top */}
+        <footer className="footer-section m-sm-top-60">
           <div className="footer-bottom" id="ocean">
             <div className="container">
               <div className="row">
