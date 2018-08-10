23115a47f234cc1135437db1d52b40b1c598119a
Edson L. Nonato Annichine <edson.annichine@netsimples.local>
2018-08-10 18:23:33 -0300

    add new item in home and finishing map layout

---
+++ app/.history/src/pages/home/styles_20180810132412.js
@@ -0,0 +1,73 @@
+import { StyleSheet } from 'react-native';
+import { general, colors, metrics} from 'styles';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    imageContainer: {
+        flex: 0.3,
+        alignItems: 'center',
+        paddingTop: metrics.baseMargin * 5,
+        marginBottom: metrics.baseMargin,
+        paddingHorizontal: metrics.basePadding,
+        backgroundColor: colors.white,
+    },
+
+    title: {
+        ...general.title,
+        textAlign: 'center',
+        fontSize: 18,
+        marginTop: metrics.baseMargin * 2,
+    },
+
+    text: {
+        ...general.text,
+        textAlign: 'center',
+        fontSize: 16,
+        color: colors.white,
+        paddingLeft: metrics.basePadding / 2,
+    },
+
+    form: {
+        flex: 0.7,
+        paddingHorizontal: metrics.basePadding,
+        paddingVertical: metrics.basePadding / 2,
+        justifyContent: 'flex-end',
+    },
+
+    subForm: {
+        flexDirection: 'row',
+    },
+
+    centerForm: {
+        alignItems: 'center',
+        height: 120,
+        marginTop: metrics.baseMargin,
+        marginHorizontal: metrics.baseMargin / 2.5,
+        borderRadius: metrics.baseRadius * 2,
+    },
+
+    imageBackground: {
+        flex: 1,
+        width: metrics.screenWidth,
+        margin: metrics.baseMargin / 3,
+        height: 100,
+    },
+
+    button: {
+        flex: 1,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        ...general.title,
+        color: colors.white,
+        textAlign: 'center',
+    },
+});
+
+export default styles;
